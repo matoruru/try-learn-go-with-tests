a0054e2be85bfac44fb56e828b73b12087f2d924
matoruru <matoruru@shgt.jp>
2024-11-27 20:10:19 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Learn Go with Testsを試す
+
+[https://quii.gitbook.io/learn-go-with-tests](https://quii.gitbook.io/learn-go-with-tests)
+
+## How to enter the development environment
+
+```
+nix develop
+```
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            fish
+            git
+            go
+          ];
+          shellHook = ''
+            exec fish
+          '';
+        };
+      }
+    );
+}
